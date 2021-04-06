478e449c0e094b98005d8704405de94fdb4dec1c
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-06 22:05:18 +0530

    OOP Demo - Product & Product Item

---
+++ CSharpLanguageFeatures/OOPDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOPDemo
+{
+    class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public decimal UnitCost { get; set; }
+    }
+
+    class ProductItem
+    {
+        public Product Product { get; set; }
+        public int Units { get; set; }
+        /*
+        public decimal ProductValue() => UnitCost * Units;
+        */
+        public decimal ProductValue()
+        {
+            return Units * Product.UnitCost;
+        }
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var productItem = new ProductItem { Product = new Product { Id = 101, Name = "Pen", UnitCost = 5}, Units = 10 };
+            Console.WriteLine($"Product Value = {productItem.ProductValue()}");
+        }
+    }
+}

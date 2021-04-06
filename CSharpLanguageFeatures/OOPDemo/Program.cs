82f591230d920b595f15962321bc5b9473c35efe
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-06 22:33:40 +0530

    Applying Discount at Order level

--- CSharpLanguageFeatures/OOPDemo/Program.cs
+++ CSharpLanguageFeatures/OOPDemo/Program.cs
@@ -6,18 +6,9 @@ namespace OOPDemo
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        private decimal _unitCost;
-        public decimal UnitCost {
-            set
-            {
-                _unitCost = value;
-            }
-            get
-            {
-                return _unitCost * ((100 - Discount) / 100);
-            }
-        }
+        public decimal UnitCost { get; set; }
         public decimal Discount { get; set; }
+        public decimal FinalCost() => UnitCost * ((100 - Discount) / 100);
        
     }
 
@@ -30,7 +21,39 @@ namespace OOPDemo
         */
         public decimal ProductValue()
         {
-            return Units * Product.UnitCost;
+            return Units * Product.FinalCost();
+        }
+    }
+
+    class Order
+    {
+        public ProductItem[] ProductItems = new ProductItem[] {
+            new ProductItem
+            {
+                Product = new Product {Id = 100, Name = "Pen", UnitCost = 10, Discount = 5}
+                , Units = 10
+            },
+            new ProductItem
+            {
+                Product = new Product {Id = 101, Name = "Pencil", UnitCost = 5, Discount = 5}
+                , Units = 10
+            },
+            new ProductItem
+            {
+                Product = new Product {Id = 102, Name = "Marker", UnitCost = 50, Discount = 10}
+                , Units = 10
+            },
+        };
+        public decimal Discount;
+
+        public decimal CalculateTotal()
+        {
+            var total = 0m;
+            foreach(var productItem in ProductItems)
+            {
+                total += productItem.ProductValue();
+            }
+            return total * ((100 - Discount) / 100);
         }
     }
 
@@ -40,9 +63,13 @@ namespace OOPDemo
     {
         static void Main(string[] args)
         {
+            /*
             var product = new Product { Id = 101, Name = "Pen", UnitCost = 5, Discount = 5 };
             var productItem = new ProductItem { Product = product, Units = 10 };
             Console.WriteLine($"Product Value = {productItem.ProductValue()}");
+            */
+            var order = new Order();
+            Console.WriteLine($"Order Value = {order.CalculateTotal()}");
         }
     }
 }

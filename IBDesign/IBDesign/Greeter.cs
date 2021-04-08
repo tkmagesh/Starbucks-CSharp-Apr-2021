3f1aec802c6453f5ad0f788a93128bbf0618561e
Magesh Kuppan <tkmagesh77@gmail.com>
2021-04-09 00:08:00 +0530

    Greeter class

---
+++ IBDesign/IBDesign/Greeter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace IBDesign
+{
+    public class Greeter
+    {
+       public string Greet(string userName)
+        {
+            if (DateTime.Now.Hour < 12)
+            {
+                return $"Hi {userName}, Good Morning!";
+            } else {
+                return $"Hi {userName}, Good Evening!";
+            }
+        }
+    }
+}
